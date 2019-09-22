bbece76d0fe0fa136980de4c65e1b4a5463e2069
SergeyOn <seregapantera@gmail.com>
2019-09-23 01:29:01 +0300

    string calc done

--- src/index.js
+++ src/index.js
@@ -4,7 +4,17 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let sum = 0;
+
+    try {
+        sum += (new Function('return ' + expr))();      
+    } catch (err) {
+        throw new Error ('ExpressionError: Brackets must be paired.');
+    }
+
+    if (sum === Infinity) throw new Error ('TypeError: Devision by zero.');
+
+    return sum;
 }
 
 module.exports = {
